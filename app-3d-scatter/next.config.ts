08e2f3c0b3398aba7edc983f20e7672c3f87794c
yo16 <xxyo1xx@gmail.com>
2025-07-04 13:50:07 +0900

    add github actions

--- app-3d-scatter/next.config.ts
+++ app-3d-scatter/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/app-3d-scatter",
+  trailingSlash: true,
 };
 
 export default nextConfig;
